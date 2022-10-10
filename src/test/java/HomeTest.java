af8e0bbe47cd672a3c2b1b30364eb48c6587f034
Nurik <nicksbastion@yahoo.com>
2022-10-10 18:03:55 -0400

    US34 completed

--- src/test/java/HomeTest.java
+++ src/test/java/HomeTest.java
@@ -1,6 +1,13 @@
+import org.testng.annotations.Test;
+
 public class HomeTest {
     String name = "Nurik";
     String role = "Student";
     int id = 99;
     String email = "nick@yahoo.com";
+
+    @Test(testName = "Testing title of the page")
+    public void test01(){
+        //implementation
+    }
 }
